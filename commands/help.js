336619bd2a10931372b511d9fa95afc3957e5ff8
Kemill <Ckemill@gmail.com>
2020-06-24 03:36:11 -0400

    Updated help url to perritu.net page.

--- commands/help.js
+++ commands/help.js
@@ -15,7 +15,7 @@ module.exports = {
         const helpEmbed = new Discord.MessageEmbed()
         .setColor("#8b3dbd")
         .setTitle(ayu)
-        .setURL('https://github.com/Ckemill/Prueba#readme')
+        .setURL('https://perritu.net')
         .addFields(
             {name: '**'+prefix+'play**', value: 'Busco una canción en youtube y la reproduzco. \n*(también acepto links de Facebook y Youtube.)*', inline: true},
             {name: '**'+prefix+'pause**', value: "Pauso la musica. \n*(Debes estar en un voice para usarlo.)*", inline: false},
@@ -27,7 +27,7 @@ module.exports = {
         )
         .setTimestamp(fecha)
         .addField('\u200b', '\u200b')
-        .setFooter('Bot made by Kémill', 'https://en.gravatar.com/userimage/184776942/b820bcb781c920db44ccc7bda2362843?size=200');
+        .setFooter('Bot made by Ckemill', 'https://en.gravatar.com/userimage/184776942/b820bcb781c920db44ccc7bda2362843?size=200');
 
         message.channel.send(helpEmbed);
 
