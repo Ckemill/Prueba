782ee95d07f321fbc036ef35c00e1c0c06ab1778
Kemill <Ckemill@gmail.com>
2020-07-16 14:18:04 -0400

    fixed errors

--- commands/perritu.js
+++ commands/perritu.js
@@ -1,37 +1,15 @@
-const db = require('../database');
-
 module.exports = {
 	name: 'test',
     description: 'test command',
 	async execute(client, message, args) {
 
-		var id = message.channel.id;
-		var name = message.guild.name;
-
-		db.query(`UPDATE wp_servers_discord SET name = "${name}" WHERE ID = ${id}`, { type: db.QueryTypes.UPDATE })
-		.then( () => {
-			console.log('Success');
-		})
-		.catch((err) => {
-			console.log(err);
-		});
-
-		db.query('SELECT * FROM wp_servers_discord', { type: db.QueryTypes.SELECT })
-		.then(function(users) {
-
-			for (var i = 0; i < users.length; i++) {
-				console.log(users[i].ID);
-			}
-
-		});
-
-		/*message.channel.send('Hello.').then(sentMessage => {
+		message.channel.send('hola.').then(sentMessage => {
 
 			message.reply(`deleting message.`).then( () => {
 				sentMessage.delete();
 			});
 
-		});*/
+		});
 
 	}
 };
