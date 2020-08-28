9e36f0a49c46b4edbd746cec19952f66a946e99b
Kemill <Ckemill@gmail.com>
2020-08-28 16:33:22 -0400

    Added message to mute and unmute commands.

--- commands/mute.js
+++ commands/mute.js
@@ -1,19 +1,23 @@
 const { User } = require("discord.js");
 
 module.exports = {
-	name: 'mute',
+  name: 'mute',
     description: 'mute all on voice',
 	async execute(client, message, args) {
-
-        if (message.member.voice.channel) {
+    
+    if (message.member.voice.channel) {
             
-            let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
-            for (const [memberID, member] of channel.members) {
-              member.voice.setMute(true);
-            }
-          } else {
-            message.reply('No estas en ningun canal de voz.');
-          }
+      let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
+      
+      const users = [];
 
+      for (const [memberID, member] of channel.members) {
+        member.voice.setMute(true);
+        users.push(member);
+      }
+      message.channel.send(`Muteando a ${users.join(', ')}.`);
+    } else {
+      message.reply('No estas en ningun canal de voz.');
+    }
 	}
 };
--- commands/unmute.js
+++ commands/unmute.js
@@ -1,19 +1,23 @@
 const { User } = require("discord.js");
 
 module.exports = {
-	name: 'unmute',
+  name: 'unmute',
     description: 'unmute all on voice',
 	async execute(client, message, args) {
-
-        if (message.member.voice.channel) {
+    
+    if (message.member.voice.channel) {
             
-            let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
-            for (const [memberID, member] of channel.members) {
-              member.voice.setMute(false);
-            }
-          } else {
-            message.reply('No estas en ningun canal de voz.');
-          }
+      let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
+      
+      const users = [];
 
+      for (const [memberID, member] of channel.members) {
+        member.voice.setMute(false);
+        users.push(member);
+      }
+      message.channel.send(`Desmuteando a ${users.join(', ')}.`);
+    } else {
+      message.reply('No estas en ningun canal de voz.');
+    }
 	}
 };
