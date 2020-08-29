81277cf765924dc4a4368ae18d6bf98629317021
Kemill <Ckemill@gmail.com>
2020-08-28 22:20:41 -0400

    now you can mute bye mentioning

--- commands/mute.js
+++ commands/mute.js
@@ -1,25 +1,59 @@
 const { User } = require("discord.js");
+const { prefix } = require("../config.json");
 
 module.exports = {
   name: 'mute',
     description: 'mute all on voice',
 	async execute(client, message, args) {
+
+    if (!message.member.permissions.has('MUTE_MEMBERS')) return message.reply(`No tienes permisos para mutear a otros.`);
+
+    if (!message.guild.me.hasPermission("MUTE_MEMBERS")) return message.reply("No tengo permisos para mutear a otros.");
+
+    if (!args.length) return message.channel.send(`Puedes mutear mencionando a quienes o a todos con **${prefix}mute all**. \nEjemplo: \`${prefix}mute @${message.author.username}\`.`);
     
     if (message.member.voice.channel) {
-            
-      let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
-      
-      const users = [];
 
-      for (const [memberID, member] of channel.members) {
-        member.voice.setMute(true);
-        users.push(member);
+      if(args.length == '1' && args=='all'){
+            
+        let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
+        
+        const users = [];
+
+        for (const [memberID, member] of channel.members) {
+          member.voice.setMute(true);
+          users.push(member);
+        }
+        message.channel.send(`Muteando a ${users.join(', ')}.`)
+        .then(msg => {
+          msg.delete({ timeout: 10000 })
+        });
       }
-      message.channel.send(`Muteando a ${users.join(', ')}.`)
-      .then(msg => {
-        msg.delete({ timeout: 10000 })
+
+      //muteando con menciones:
+
+      else if (!message.mentions.members.first()) return message.reply(`Puedes mutear mencionando a quienes o a todos con **${prefix}mute all**. \nEjemplo: \`${prefix}mute @${message.author.username}\`.`);
+
+      message.mentions.members.forEach(async (user)=>{
+
+        if (!user) return message.reply("menciona a quien o quienes quieres mutear.");
+
+        if (user.id === message.author.id) return message.reply("pero para eso muteate tu mismo...");
+
+        //if (user.roles.cache.has(muterole)) return message.reply(`${user} ya está muteado.`); detectar si esta muteado.
+
+        if (!user.voice.channel) return message.reply(`${user} no está en ningún voice.`);
+
+        if (user.voice.channel.id != message.member.voice.channel.id) return message.reply(`${user} no está en el mismo voice que tu.`);
+
+        user.voice.setMute(true);
+        
+        await message.channel.send(`${user}, te acaba de mutear ${message.author}.`);
+
       });
-    } else {
+
+    }
+    else {
       message.reply('No estas en ningun canal de voz.');
     }
 	}
--- commands/unmute.js
+++ commands/unmute.js
@@ -1,25 +1,55 @@
 const { User } = require("discord.js");
+const { prefix } = require("../config.json");
 
 module.exports = {
   name: 'unmute',
-    description: 'unmute all on voice',
+    description: 'mute all on voice',
 	async execute(client, message, args) {
+
+    if (!message.member.permissions.has('MUTE_MEMBERS')) return message.reply(`No tienes permisos para desmutear a otros.`);
+
+    if (!message.guild.me.hasPermission("MUTE_MEMBERS")) return message.reply("No tengo permisos para desmutear a otros.");
+
+    if (!args.length) message.member.voice.setMute(false);
     
     if (message.member.voice.channel) {
-            
-      let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
-      
-      const users = [];
 
-      for (const [memberID, member] of channel.members) {
-        member.voice.setMute(false);
-        users.push(member);
+      if(args.length == '1' && args=='all'){
+            
+        let channel = message.guild.channels.cache.get(message.member.voice.channel.id);
+      
+        const users = [];
+
+        for (const [memberID, member] of channel.members) {
+          member.voice.setMute(false);
+          users.push(member);
+        }
+        message.channel.send(`Desmuteando a ${users.join(', ')}.`)
+        .then(msg => {
+          msg.delete({ timeout: 10000 })
+        });
       }
-      message.channel.send(`Desmuteando a ${users.join(', ')}.`)
-      .then(msg => {
-        msg.delete({ timeout: 10000 })
+
+      //muteando con menciones:
+
+      message.mentions.members.forEach(async (user)=>{
+
+        if (!user) return message.reply("menciona a quien o quienes quieres mutear.");
+
+        //if (user.roles.cache.has(muterole)) return message.reply(`${user} ya está muteado.`); detectar si esta muteado.
+
+        if (!user.voice.channel) return message.reply(`${user} no está en ningún voice.`);
+
+        if (user.voice.channel.id != message.member.voice.channel.id) return message.reply(`${user} no está en el mismo voice que tu.`);
+
+        user.voice.setMute(false);
+        
+        await message.channel.send(`${user}, te acaba de desmutear ${message.author}.`);
+
       });
-    } else {
+
+    }
+    else {
       message.reply('No estas en ningun canal de voz.');
     }
 	}
